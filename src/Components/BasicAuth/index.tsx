30d04cd36ffa1c181dc28edc22b3b1a6ec00f589
chano08 <christianraymondpalis@gmail.com>
2021-11-03 23:04:47 +0800

    init

---
+++ src/Components/BasicAuth/index.tsx
@@ -0,0 +1,80 @@
+
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import cx from "classnames";
+
+const Schema = Yup.object().shape({
+    email: Yup.string().required("Required").email(),
+    password: Yup.string().required("Required"),
+});
+
+const BasicAuth = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validateOnChange: false,
+        validationSchema: Schema,
+        onSubmit: async (
+            values,
+            { setFieldError, setSubmitting, resetForm }
+        ) => {
+            console.log('should submit here')
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div>
+                <label className="label">
+                    <span className="label-text">Email</span>
+                </label>
+                <input
+                    disabled={formik.isSubmitting}
+                    value={formik.values.email}
+                    onChange={formik.handleChange("email")}
+                    type="email"
+                    placeholder="Email"
+                    className={cx("input input-bordered w-full", {
+                        "border-red-400": formik.errors.email,
+                    })}
+                />
+                {formik.errors.email && (
+                    <div className="text-red-500">{formik.errors.email}</div>
+                )}
+            </div>
+            <div className="mt-5">
+                <label className="label">
+                    <span className="label-text">Password</span>
+                </label>
+                <input
+                    disabled={formik.isSubmitting}
+                    value={formik.values.password}
+                    onChange={formik.handleChange("password")}
+                    type="password"
+                    placeholder="Password"
+                    className={cx("input input-bordered w-full", {
+                        "border-red-400": formik.errors.password,
+                    })}
+                />
+                {formik.errors.password && (
+                    <div className="text-red-500">{formik.errors.password}</div>
+                )}
+            </div>
+            <div className="mt-5 flex justify-end">
+                <button
+                    className={cx("btn btn-primary", {
+                        loading: formik.isSubmitting,
+                    })}
+                    type="submit"
+                >
+                    Login
+                </button>
+            </div>
+        </form>
+    );
+};
+
+export default BasicAuth;
---
+++ src/Pages/Secure/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../Components/Layout";
+
+const SecurePage = () => {
+    return (
+        <Layout>
+            <div>this is a secure page</div>
+        </Layout>
+    );
+};
+
+export default SecurePage;
